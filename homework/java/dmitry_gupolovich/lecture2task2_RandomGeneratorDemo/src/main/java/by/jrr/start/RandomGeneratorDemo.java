9e27f0e368964c3de03ca42fb08ec90560c68548
fse-prosto <prostoxdsmile@gmail.com>
2021-03-21 21:14:49 +0300

    Completed task RandomGeneratorDemo / lecture2task2

---
+++ homework/java/dmitry_gupolovich/lecture2task2_RandomGeneratorDemo/src/main/java/by/jrr/start/RandomGeneratorDemo.java
@@ -0,0 +1,22 @@
+package by.jrr.start;
+
+import java.util.Arrays;
+
+/**
+ * 1. Сгенерировать 3 случайных числа и вывести их на экран.
+ * 2. Посчитать сумму этих чисел и вывести результат на экран.
+ */
+public class RandomGeneratorDemo {
+    public static void main(String[] args) {
+        int array[] = new int[3];
+        for (int i = 0; i < 3 ; i++) {
+            array[i] = (int) (Math.random()*500);
+        }
+
+        Arrays.stream(array).forEach(x -> System.out.printf("%s ", x));
+
+        System.out.println("\nSum array is = "+Arrays.stream(array).sum());
+
+    }
+}
+
