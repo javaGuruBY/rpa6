8efdf1219d5ec65be50999ff9f43a2e7a0927f9e
Kruz Uzdyak <kruzuzdyak.96@gmail.com>
2021-03-31 11:50:32 +0300

    adds checkUserPassword functionality

--- homework/java/evgeni_volodko/lecture4/src/main/java/ku/loginfeature/service/LoginService.java
+++ homework/java/evgeni_volodko/lecture4/src/main/java/ku/loginfeature/service/LoginService.java
@@ -1,5 +1,10 @@
 package ku.loginfeature.service;
 
+import ku.loginfeature.bin.User;
+
 public class LoginService {
 
+    public boolean checkUserPassword(User user, String userInput) {
+        return user.getPassword().equals(userInput);
+    }
 }
--- homework/java/evgeni_volodko/lecture4/src/test/java/ku/loginfeature/service/LoginServiceTest.java
+++ homework/java/evgeni_volodko/lecture4/src/test/java/ku/loginfeature/service/LoginServiceTest.java
@@ -1,7 +1,43 @@
 package ku.loginfeature.service;
 
-import static org.junit.Assert.*;
+import ku.loginfeature.bin.User;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class LoginServiceTest {
 
+    LoginService loginService;
+
+    @Test
+    public void checkUserPasswordPositive(){
+        User user = getUser();
+
+        String userInput = "qwerty";
+
+        boolean actual = loginService.checkUserPassword(user, userInput);
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void checkUserPasswordNegative(){
+        User user = getUser();
+
+        String userInput = "not qwerty";
+
+        boolean actual = loginService.checkUserPassword(user, userInput);
+        Assert.assertFalse(actual);
+    }
+
+    @Before
+    public void setUp(){
+       this.loginService = new LoginService();
+    }
+
+
+    private User getUser() {
+        User user = new User();
+        user.setPassword("qwerty");
+        return user;
+    }
 }
